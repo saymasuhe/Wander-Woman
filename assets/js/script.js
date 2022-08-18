443405a7f9efa75dde5d3bdb9948babe9380cc23
saymasuhe <saymasuhe911@gmail.com>
2022-08-18 15:22:11 +0600

    -

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$('.carousel').carousel({
+    interval: 4000
+});
+
+window.addEventListener('scroll', function(){
+    const hr = document.querySelector('.top-navbar');
+    hr.classList.toggle("sticky", window.scrollY > 0);
+});
+
+$(function(){
+    $('.mhn-slide').owlCarousel({
+        nav:true,
+        slideBy:1,
+        rewind:false,
+        responsive:{
+            0:{items:1},
+            480:{items:2},
+            600:{items:3},
+        },
+        smartSpeed:200,
+        navText:['<img src="assets/images/prev.png" alt="" srcset="">','<img src="assets/images/next.png" alt="" srcset="">']
+    });
+});
+
+$(document).ready(function(){
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1200
+    });
+  });
